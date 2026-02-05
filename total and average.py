3907cd1c2c776c01df11b6075848b0531917e54a
Manasa_Vepati <manasavepati07@gmail.com>
2026-02-05 20:51:03 +0530

    Add files via upload

---
+++ total and average.py
@@ -0,0 +1,8 @@
+#total and average
+a=float(input("enter first number:"))
+b=float(input("enter second number:"))
+c=float(input("enter third numbers:"))
+total = a+b+c
+average=total/3
+print("total",total)
+print("average",average)
